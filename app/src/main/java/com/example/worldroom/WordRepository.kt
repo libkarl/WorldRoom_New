5f8b076910978b19a1fd02e64f55fa25b427453a
student <student@example.com>
2021-11-23 11:32:23 +0100

    Create the View model

--- app/src/main/java/com/example/worldroom/WordRepository.kt
+++ app/src/main/java/com/example/worldroom/WordRepository.kt
@@ -5,6 +5,8 @@ import androidx.annotation.WorkerThread
 import kotlinx.coroutines.flow.Flow
 
 class WordRepository {
+    val allWords: Any
+
     // Declares the DAO as a private property in the constructor. Pass in the DAO
 // instead of the whole database, because you only need access to the DAO
     class WordRepository(private val wordDao: WordDao) {
